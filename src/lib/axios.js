50691a7c9b1794ad43668b49ebdb7dfb8e8cc76a
b3rking <ishimweeschylebersy@gmail.com>
2026-06-30 10:49:45 +0200

    update the axios instance

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,7 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 60 * 1000,
     headers: { "Accept": "application/json", "Content-Type": "application/json" },
+    withCredentials: false,
 });
 
 api.interceptors.request.use((config) => {
